9be3e0a1232aa937ad542b4303d19ce3b91147e5
Chad Fowler <chad@chadfowler.com>
2016-10-24 18:32:15 -0500

    Support Mac OS X

--- ipfs-screen.sh
+++ ipfs-screen.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
+mkdir -p ~/.ipfs-screen/
 echo "testing" >> ~/.ipfs-screen/ipfs-add.log
 file=ipfs-screen.jpg
 
-gnome-screenshot -a -f ~/.ipfs-screen/$file
+platform=`uname`
+if [[ "$platform" == "Darwin" ]]; then
+  screencapture -t jpg -i ~/.ipfs-screen/$file
+else
+  gnome-screenshot -a -f ~/.ipfs-screen/$file
+fi
 
 hash_log=$(ipfs add ~/.ipfs-screen/$file)
 
 echo $hash_log >> ~/.ipfs-screen/ipfs-add.log
 
-
 hash=$(echo $hash_log | awk '{ print $2; }')
 
-echo "https://ipfs.io/ipfs/$hash" | xclip -selection clipboard
+clipboard_command="xclip -selection clipboard"
+
+if [[ "$platform" == "Darwin" ]]; then
+  clipboard_command="pbcopy"
+fi
+
+echo "https://ipfs.io/ipfs/$hash" | `$clipboard_command`
